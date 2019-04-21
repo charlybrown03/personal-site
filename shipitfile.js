959bd2e15b55d0f7e272b6fd9fecc6a8ca31e020
Carlos Moreno Villodre <carlos.moreno.villodre@gmail.com>
2019-04-21 16:47:09 +0200

    Build locally and deploy dist folder

--- shipitfile.js
+++ shipitfile.js
@@ -1,20 +1,23 @@
-const SERVER_PATH = '/home/carlos/personal-site'
-
 module.exports = shipit => {
   require('shipit-deploy')(shipit)
 
   shipit.initConfig({
     default: {
-      deployTo: SERVER_PATH,
+      dirToCopy: 'dist',
+      deployTo: '/home/carlos/personal-site',
       repositoryUrl: 'git@github.com:charlybrown03/personal-site.git',
     },
     prod: {
       servers: 'carlos@carlosmoreno.dev',
-      workspace: SERVER_PATH,
     },
   })
 
-  shipit.on('deployed', async () => {
-    await shipit.remote(`cd ${SERVER_PATH}/current && npm i && npm run generate`)
+  shipit.on('fetched', () => {
+    return shipit.start('build')
+  })
+
+  shipit.blTask('build', async () => {
+    await shipit.local('npm i', { cwd: shipit.workspace })
+    await shipit.local('npm run generate', { cwd: shipit.workspace })
   })
 }
