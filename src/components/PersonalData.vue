72d3df0de7d3aabc2cc829e16ffe1e765d1ded0a
Carlos Moreno Villodre <carlos.moreno.villodre@gmail.com>
2019-05-10 21:07:35 +0200

    Use webp profile image with jpeg fallback

--- src/components/PersonalData.vue
+++ src/components/PersonalData.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="personal-data">
-    <img
-      src="~/assets/images/profile.jpeg"
-      class="personal-data__image"
-      alt="Personal image"
-    >
+    <picture>
+      <source srcset="~/assets/images/profile.webp" type="image/webp">
+      <source srcset="~/assets/images/profile.jpeg" type="image/jpeg">
+      <img
+        src="~/assets/images/profile.jpeg"
+        class="personal-data__image"
+        alt="Personal image"
+      >
+    </picture>
     <h1 class="personal-data__title" v-text="$t('home.title')" />
     <h2 class="personal-data__subtitle" v-text="$t('home.subtitle')" />
   </div>
