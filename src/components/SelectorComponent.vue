172f79f747d5a8ac2323a1bd1d017151396f00cb
Carlos Moreno Villodre <carlos.moreno.villodre@gmail.com>
2019-05-15 20:42:45 +0200

    Create selectors page prototype

---
+++ src/components/SelectorComponent.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <span
+      @click="isDefault = !isDefault"
+      v-text="computedText"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SelectorComponent',
+
+  props: {
+    selector: {
+      type: String,
+      required: true,
+    },
+  },
+
+  data () {
+    return {
+      defaultText: '¿Cómo llegar hasta mí?',
+      isDefault: true,
+    }
+  },
+
+  computed: {
+    computedText () {
+      return this.isDefault ? this.defaultText : this.selector
+    },
+  },
+}
+</script>
+
+<style scoped>
+  span {
+    display: block;
+    cursor: pointer;
+    padding: 1rem;
+  }
+</style>
---
+++ src/pages/selectors.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="selectors">
+    <div class="gtm__1">
+      <selector-component
+        class="gtm__2"
+        :selector="'.gtm__1 .gtm__2 o .gtm__1 > .gtm__2'"
+      />
+    </div>
+    <div class="gtm__1">
+      <div class="gtm__3">
+        <selector-component
+          class="gtm__2"
+          :selector="'.gtm__1 .gtm__2 o .gtm__3 > .gtm__2 o .gtm__1 .gtm__3 > .gtm__2'"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import SelectorComponent from '@/components/SelectorComponent'
+
+export default {
+  name: 'Selectors',
+
+  components: {
+    SelectorComponent,
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+  .selectors {
+    display: flex;
+  }
+</style>
